4755cdb58bcc144cd6b3c0e7534f0eb5ee0c8b08
sahajsikri-glitch <sahajsikri@gmail.com>
2026-07-22 11:48:35 +0530

    structuring

--- ABES-college/clg.cpp
+++ ABES-college/clg.cpp
@@ -142,54 +142,54 @@ class student{
 // 1. anual income 2. salary after 10% deduction as tax
 // 3.salary after 5& deduction in another service 
 
-class employees{
-  private:
-  int id;
-  char name[20];
-  float salary;
-  char dept[10];
+// class employees{
+//   private:
+//   int id;
+//   char name[20];
+//   float salary;
+//   char dept[10];
 
-  public:
-  void get_info(){
-    cout<<"enter your id: ";
-    cin>>id;
+//   public:
+//   void get_info(){
+//     cout<<"enter your id: ";
+//     cin>>id;
 
-    cin.ignore();//for ignoring the leftover newline
-    cout<<"enter your department: ";
-    cin.getline(dept,10);
+//     cin.ignore();//for ignoring the leftover newline
+//     cout<<"enter your department: ";
+//     cin.getline(dept,10);
 
-    cout<<"enter your name: ";
-    cin.getline(name,20);
+//     cout<<"enter your name: ";
+//     cin.getline(name,20);
 
-    cout<<"enter your monthly salary: " ;
-    cin>>salary;
-  }
+//     cout<<"enter your monthly salary: " ;
+//     cin>>salary;
+//   }
   
-  void display_info(){
-    int anual_salary = salary*12;
-    cout<<"your anual salary";
-    cout<<anual_salary<<endl;
-    cout<<"salary after 10% deduction: ";
-    int x= (anual_salary*10)/100;
-    anual_salary= anual_salary-x;
-    cout<<anual_salary<<endl;
-    cout<<"salary after 5% deduction: ";
-    int y = (anual_salary*5)/100;
-    anual_salary=anual_salary-y;
-    cout<<anual_salary<<endl;
-  }
-};
+//   void display_info(){
+//     int anual_salary = salary*12;
+//     cout<<"your anual salary";
+//     cout<<anual_salary<<endl;
+//     cout<<"salary after 10% deduction: ";
+//     int x= (anual_salary*10)/100;
+//     anual_salary= anual_salary-x;
+//     cout<<anual_salary<<endl;
+//     cout<<"salary after 5% deduction: ";
+//     int y = (anual_salary*5)/100;
+//     anual_salary=anual_salary-y;
+//     cout<<anual_salary<<endl;
+//   }
+// };
 
-int main(){
-  employees emp[5];
-  cout<<"enter details of 5 employees"<<endl;
-  for(int i=0;i<5;i++){
-    emp[i].get_info();
-  }
+// int main(){
+//   employees emp[5];
+//   cout<<"enter details of 5 employees"<<endl;
+//   for(int i=0;i<5;i++){
+//     emp[i].get_info();
+//   }
 
-  cout<<"employee details";
-  for(int i=0;i<5;i++){
-    emp[i].display_info();
-  }
-  return 0;
-}
+//   cout<<"employee details";
+//   for(int i=0;i<5;i++){
+//     emp[i].display_info();
+//   }
+//   return 0;
+// }
