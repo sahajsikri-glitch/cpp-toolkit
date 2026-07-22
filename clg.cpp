cdcf87bac3f891433b588f5c851909439b4be666
sahajsikri-glitch <sahajsikri@gmail.com>
2026-07-22 10:30:05 +0530

    understanding classes and objects

--- clg.cpp
+++ clg.cpp
@@ -97,17 +97,16 @@ using namespace std;
 // 1. using namespace std which allow us to not
 //  to use std everytime in program
 // 2.helps in accessing and declaring functions if its same
-// namespace <>
-// nsmespace abc{
+// namespace abc{
 //   void display(){
 //     cout<<"hello";
 //   }
-// }
+// };
 // namespace xyz{
 //   void display(){
 //     cout<<"bye";
 //   }
-// }
+// };
 // int main(){
 //   abc::display();
 //   xyz::display();
@@ -137,3 +136,60 @@ class student{
     cout<<dept;
   }
 };
+
+//Q) write a program to show 5 diff employee information as -: 
+// id name dept and monthly salary display each employee info as
+// 1. anual income 2. salary after 10% deduction as tax
+// 3.salary after 5& deduction in another service 
+
+class employees{
+  private:
+  int id;
+  char name[20];
+  float salary;
+  char dept[10];
+
+  public:
+  void get_info(){
+    cout<<"enter your id: ";
+    cin>>id;
+
+    cin.ignore();//for ignoring the leftover newline
+    cout<<"enter your department: ";
+    cin.getline(dept,10);
+
+    cout<<"enter your name: ";
+    cin.getline(name,20);
+
+    cout<<"enter your monthly salary: " ;
+    cin>>salary;
+  }
+  
+  void display_info(){
+    int anual_salary = salary*12;
+    cout<<"your anual salary";
+    cout<<anual_salary<<endl;
+    cout<<"salary after 10% deduction: ";
+    int x= (anual_salary*10)/100;
+    anual_salary= anual_salary-x;
+    cout<<anual_salary<<endl;
+    cout<<"salary after 5% deduction: ";
+    int y = (anual_salary*5)/100;
+    anual_salary=anual_salary-y;
+    cout<<anual_salary<<endl;
+  }
+};
+
+int main(){
+  employees emp[5];
+  cout<<"enter details of 5 employees"<<endl;
+  for(int i=0;i<5;i++){
+    emp[i].get_info();
+  }
+
+  cout<<"employee details";
+  for(int i=0;i<5;i++){
+    emp[i].display_info();
+  }
+  return 0;
+}
